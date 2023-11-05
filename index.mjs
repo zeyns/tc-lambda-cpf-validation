c9e5c1612abb84ff59c422ce7b9f65f5a8f2538e
Marcos <marcoslucsrios@gmail.com>
2023-11-05 14:28:16 -0300

    getting database data from env

--- index.mjs
+++ index.mjs
@@ -2,11 +2,11 @@ const { Client } = require('pg');
 const jwt = require('jsonwebtoken');
 
 const client = new Client({
-  user: '',
-  host: '',
-  database: '',
-  password: '',
-  port: 6432
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT
 })
 
 const JWT_SECRET_KEY = 'asl292@_asda2rg546'
@@ -15,6 +15,14 @@ exports.handler = async event => {
     const headers =  {
       'Content-Type': 'application/json',
     }
+
+    const responseReturn = (statusCode, body = null) => {
+      if (body) {
+        return { statusCode, headers, body }
+      }
+      return { statusCode, headers }
+    }
+
     const body = event.body;
     if(body.document) {
       client.connect(async err => {
@@ -23,23 +31,11 @@ exports.handler = async event => {
 
           if(rows && rows.length > 0) {
             const token = jwt.sign({ customerId: res.rows[0].id }, JWT_SECRET_KEY, { algorithm: 'HS256' });
-            return  {
-              statusCode: 200,
-              headers,
-              body: {
-                token
-              }
-            }
-          } 
-          return {
-            statusCode: 401,
-            headers
+            return responseReturn(200, { token });
           }
+          
+          return responseReturn(401);
       });
     }
-    return {
-      statusCode: 400,
-      headers
-    }
-
+    return responseReturn(400);
 };
